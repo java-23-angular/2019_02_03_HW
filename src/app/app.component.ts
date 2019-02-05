68bff146e81b6823a4f6edb22b9d2e2014c4cb77
Gregory Sheygam <sheygam@gmail.com>
2019-02-05 11:24:43 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Java23190203HW';
+  contacts: Contact[] = [];
+  currName = '';
+  currEmail = '';
+  currPhone = '';
+  currAddress = '';
+
+  currentContact: Contact|null = null;
+  currentIndex: number = -1;
+
+  addContact(): void{
+    this.contacts.push({
+      name:this.currName,
+      email: this.currEmail,
+      phone:this.currPhone,
+      address:this.currAddress
+    });
+    this.currName = '';
+    this.currEmail = '';
+    this.currPhone = '';
+    this.currAddress = '';
+  }
+
+  remove(indx: number):void{
+    this.contacts.splice(indx,1);
+    if(indx === this.currentIndex){
+      this.currentContact = null;
+    }
+  }
+
+  selectCurrent(contact: Contact, indx: number): void{
+    this.currentContact = contact;
+    this.currentIndex = indx;
+  }
+}
+
+type Contact = {
+  name: string,
+  email: string,
+  phone: string,
+  address: string
 }
